4ad2cf92d1435f60fb7fd86337eee529c212962a
emilytaylorucf <emilyetaylor1020@knights.ucf.edu>
2021-03-07 10:05:57 -0500

    app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import DurationExercise from "./components/DurationExercise";
+import RepetitionExercise from "./components/RepetitionExercise";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MENU = "menu"
+const DURATION = "duration"
+const REPETITION = "repetition"
+
+class MenuScreen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { currentScreen: MENU, selectedItem: undefined }
+  }
+  render() {
+    let exercises = [
+      { exName: "Sprint", exType: "duration" },
+      { exName: "Jog", exType: "duration" },
+      { exName: "Jumping Jacks", exType: "repetition" },
+      { exName: "Sit Ups", exType: "repetition" }
+    ]
+
+    let screen
+    switch (this.state.currentScreen) {
+      case MENU:
+        screen = (<>
+          <h2>Exercise Time!</h2>
+          <>
+            {exercises.map((exercise) =>
+              exercise.exType == "repetition" ? (
+                <button onClick={() => this.setState({ currentScreen: REPETITION, selectedItem: exercise })}>{exercise.exName}</button>
+              ) : (
+                  <button onClick={() => this.setState({ currentScreen: DURATION, selectedItem: exercise })}>{exercise.exName}</button>
+                )
+
+            )}
+          </>
+        </>)
+        break
+      case DURATION:
+        screen = <DurationExercise {...this.state.selectedItem}></DurationExercise>
+        break
+      case REPETITION:
+        screen = <RepetitionExercise {...this.state.selectedItem}></RepetitionExercise>
+        break
+    }
+    return screen
+  }
 }
 
-export default App;
+export default MenuScreen
